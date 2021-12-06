61f7fa8c5f23e0970b1c813671594f43b73d1df5
JohnnytheShark <orellanajonathan7@gmail.com>
2021-12-06 10:03:11 -0500

    Added form logic

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from "react";
 import Spinner from './Components/UI/Spinners/Spinner';
 import Button from './Components/UI/Button/Button';
 import Navigation from './Components/UI/Navigation/Navigation';
+import Card from './Components/UI/Card/Card';
+
+import './App.scss';
 
 function App() {
   return (
     <React.Fragment>
     <Navigation /> 
+    <Card>
       <Button>Button Example</Button>
       <Spinner></Spinner>
+    </Card>
     </React.Fragment>
   );
 }
